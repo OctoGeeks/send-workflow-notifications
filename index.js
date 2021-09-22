037a70d46a21db62791de2097578caa1993352c2
Dylan Smith <dylan-smith@github.com>
2021-09-22 18:32:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,4 @@ catch(err) {
   process.exitCode = 1;
   const msg = err.toString().replace('%', '%25').replace('\r', '%0D').replace('\n', '%0A');
   process.stdout.write("::error::" + msg + os.EOL);
-  process.stdout.write("err.status: " + err.status.toString() + os.EOL);
-  process.stdout.write("err.message: " + err.message.toString() + os.EOL);
-  process.stdout.write("err.stdout: " + err.stdout.toString() + os.EOL);
-  process.stdout.write("err.stderr: " + err.stderr.toString() + os.EOL);
 }
