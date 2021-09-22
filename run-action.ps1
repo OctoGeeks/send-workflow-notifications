1cd853088c6aedd66f8a8582facb40d9e8c898e5
Dylan Smith <dylan-smith@github.com>
2021-09-22 05:57:11 +0000

    updated code to use new JS pattern

--- run-action.ps1
+++ run-action.ps1
@@ -1,24 +1,19 @@
-Get-ChildItem env:* | sort-object name
-
 if($IsWindows -eq $True)
 {
-    $filePath = Join-Path $PSScriptRoot "/dist/win-x64/SendWorkflowNotifications.exe"
-    Write-Output "Running $filePath..."
-    & $filePath
+    $filePath = Join-Path $PSScriptRoot "/dist/win-x64/dotnet-xplat-action.exe"
 }
 
 if($IsLinux -eq $True)
 {
-    $filePath = Join-Path $PSScriptRoot "/dist/linux-x64/SendWorkflowNotifications"
+    $filePath = Join-Path $PSScriptRoot "/dist/linux-x64/dotnet-xplat-action"
     chmod +x $filePath
-    Write-Output "Running $filePath..."
-    & $filePath
 }
 
 if($IsMacOS -eq $True)
 {
-    $filePath = Join-Path $PSScriptRoot "/dist/osx-x64/SendWorkflowNotifications"
+    $filePath = Join-Path $PSScriptRoot "/dist/osx-x64/dotnet-xplat-action"
     chmod +x $filePath
-    Write-Output "Running $filePath..."
-    & $filePath
 }
+
+Write-Output "Running $filePath..."
+& $filePath
