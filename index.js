3b2b7a7325aed68259af081b75e469754ababf84
Dylan Smith <dylan-smith@github.com>
2021-09-22 18:28:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ try {
   execSync('./run-action.ps1', { stdio: 'inherit', shell: "pwsh" });
 } 
 catch(err) {
-  process.exitcode = 1;
+  process.exitCode = 1;
   const msg = err.toString().replace('%', '%25').replace('\r', '%0D').replace('\n', '%0A');
   process.stdout.write("::error::" + msg + os.EOL);
+  process.stdout.write("err.status: " + err.status.toString() + os.EOL);
+  process.stdout.write("err.message: " + err.message.toString() + os.EOL);
+  process.stdout.write("err.stdout: " + err.stdout.toString() + os.EOL);
+  process.stdout.write("err.stderr: " + err.stderr.toString() + os.EOL);
 }
-
-process.exitcode = 1;
-process.stdout.write("::error::" + "foo" + os.EOL);
