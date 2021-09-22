14a15649a681f9129668ad336bb28a1969e79e53
Dylan Smith <dylan-smith@github.com>
2021-09-22 18:09:21 -0500

    adding error handling

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const { execSync } = require('child_process');
 
-execSync('./run-action.ps1', { stdio: 'inherit', shell: "pwsh" });
+try {
+  execSync('./run-action.ps1', { stdio: 'inherit', shell: "pwsh" });
+} 
+catch(err) {
+  process.exitcode = 1;
+  const msg = err.toString().replace('%', '%25').replace('\r', '%0D').replace('\n', '%0A');
+  process.stdout.write("::error::" + msg);
+}
